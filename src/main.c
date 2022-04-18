082906a5240148400432e1751de79063ad775fa5
SofiaKats <sofiakatsaouni12@gmail.com>
2022-04-18 17:20:40 +0300

    addition of comments

--- src/main.c
+++ src/main.c
@@ -9,26 +9,28 @@
 
 int main(int argc, char *argv[])
 {
-  pid_t  pid;
-  int fd[2];
+   pid_t  pid;
+   int fd[2];
 
-  if (pipe(fd) == -1){ perror("pipe"); exit(1); }
-  if ( (pid = fork()) == -1 ){ perror("fork"); exit(1); }
+   // Creation of unamed pipe
+   if (pipe(fd) == -1){ perror("failed to create pipe.\n"); exit(1); }
+   // Creation of listener process
+   if ( (pid = fork()) == -1 ){ perror("failed to fork.\n"); exit(1); }
 
    // Manager Process
    if (pid) {
       close(fd[WRITE]);
-      dup2(fd[READ],0); // 0 : standard input (stdin)
+      dup2(fd[READ],0); // read pipe will become stdin(0)
       close(fd[READ]);
    }
    // Listener Process
    else
    {
       close(fd[READ]);
-      dup2(fd[WRITE],1); // 1 : standard output (stdout)
+      dup2(fd[WRITE],1); // stdout(1) of exec'd inotifywait will write data to pipe
       close(fd[WRITE]);
       execlp("inotifywait", "inotifywait", "-m", "example", (char*)NULL);
-      perror("execlp");
+      perror("execlp failed.\n");
    }
 
    return 0;
