17aac89ddc43f0fc39fa466ccdcd878d545516a1
SofiaKats <sofiakatsaouni12@gmail.com>
2022-04-18 16:52:54 +0300

    listener and manager processes created, unamed pipes created, execpl done

--- version2/src/main.c
+++ version2/src/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
-int main(int argc, char** argv)
+#define  READ 0
+#define  WRITE 1
+
+int main(int argc, char *argv[])
 {
-    printf("hi i'm functional!\n");
-    
-    return 0;
+  pid_t  pid;
+  int fd[2];
+
+  if (pipe(fd) == -1){ perror("pipe"); exit(1); }
+  if ( (pid = fork()) == -1 ){ perror("fork"); exit(1); }
+
+   // Manager Process
+   if (pid) {
+      close(fd[WRITE]);
+      dup2(fd[READ],0); // 0 : standard input (stdin)
+      close(fd[READ]);
+   }
+   // Listener Process
+   else
+   {
+      close(fd[READ]);
+      dup2(fd[WRITE],1); // 1 : standard output (stdout)
+      close(fd[WRITE]);
+      execlp("inotifywait", "inotifywait", "-m", "example", (char*)NULL);
+      perror("execlp");
+   }
+
+   return 0;
 }
+
