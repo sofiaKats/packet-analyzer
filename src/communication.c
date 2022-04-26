36a6f1c3ede61e1e446e0b3f6c78d92da02e4425
SofiaKats <sofiakatsaouni12@gmail.com>
2022-04-26 22:40:20 +0300

    re-arranging code

--- src/communication.c
+++ src/communication.c
@@ -1,7 +1,6 @@
 #include "communication.h"
 
-void create_fifos(int no_fifo, char** fifo1, char** fifo2)
-{
+void create_fifos(int no_fifo, char** fifo1, char** fifo2) {
     // Create a unique name (e.g manager_f_read_1 , manager_f_write_6 )
 	sprintf(*fifo1, "%s%d", "manager_f_read_", no_fifo);
     if ( (mkfifo(*fifo1, PERMS) < 0) && (errno != EEXIST) ) {
@@ -23,8 +22,7 @@ int open_fifo(char* fifo, int action) {
    return value;
 }
 
-void send_filename_to_worker(char* filename, char* fifo1, char* fifo2)
-{
+void send_filename_to_worker(char* filename, char* fifo1, char* fifo2) {
     int readfd, writefd;
     /* Created the FIFOs, now open them -- one for
     * reading and one for writing.
@@ -46,8 +44,7 @@ void send_filename_to_worker(char* filename, char* fifo1, char* fifo2)
     close(writefd);
 }
 
-void receive_filename_from_manager(char* fifo1, char* fifo2)
-{
+void receive_filename_from_manager(char* fifo1, char* fifo2) {
     int readfd, writefd;
     /* Open the FIFOs.  We assume server has already created them.  */
     writefd = open_fifo(fifo1, WRITE);
@@ -68,16 +65,14 @@ void receive_filename_from_manager(char* fifo1, char* fifo2)
     close(writefd);
 }
 
-void unlink_fifo(char* fifo)
-{
+void unlink_fifo(char* fifo) {
     /* Delete the FIFO, now that we're done.  */
     if ( unlink(fifo) < 0) {
         perror("can't unlink fifo\n");
     }
 }
 
-void extract_filename(char* buffer, char** temp)
-{
+void extract_filename(char* buffer, char** temp) {
     char *token = strtok(buffer, " "); 
     while( token != NULL ) {
         strcpy(*temp,token);     // last loop will store filename
