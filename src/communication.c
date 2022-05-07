c8d5e37faee3b0e2a27dfc1ff95234cb5634f286
Sophia Katsaouni <76869852+sofiaKats@users.noreply.github.com>
2022-05-07 18:52:21 +0300

    creation and deletion of directory for named pipes all done

--- src/communication.c
+++ src/communication.c
@@ -1,13 +1,22 @@
 #include "communication.h"
 
 void create_fifos(int no_fifo, char** fifo1, char** fifo2) {
+    // if it's the first time a fifo is created, make a directory to store fifos to
+    if(no_fifo==1) {
+        char dir_path[] = "named_fifos";
+        DIR* dir = opendir(dir_path);
+        if (dir)                            // If dir already exists 
+            delete_dir(dir_path);           // completely remove it
+        
+        mkdir(dir_path, 0777);
+    }
     // Create a unique name (e.g manager_f_read_1 , manager_f_write_6 )
-	sprintf(*fifo1, "%s%d", "manager_read_", no_fifo);
+	sprintf(*fifo1, "%s%d", "named_fifos/manager_read_", no_fifo);
     if ( (mkfifo(*fifo1, PERMS) < 0) && (errno != EEXIST) ) {
        perror("can't create fifo");
    }
 
-   sprintf(*fifo2, "%s%d", "manager_write_", no_fifo);
+   sprintf(*fifo2, "%s%d", "named_fifos/manager_write_", no_fifo);
    if ((mkfifo(*fifo2, PERMS) < 0) && (errno != EEXIST)) {
        unlink_fifo(*fifo1);
        perror("can't create fifo");
@@ -77,4 +86,24 @@ void extract_filename(char* buffer, char** temp) {
         strcpy(*temp,token);     // last loop will store filename
         token = strtok(NULL, " ");
     }
+}
+
+void delete_dir(char* directory) {
+    struct dirent *dr;
+    char file_path[350];
+    DIR* dir;
+    if ((dir = opendir (directory)) != NULL) {
+        // read directory and delete all existing files in direcotry
+        while ((dr = readdir (dir)) != NULL) {
+            //printf ("%s\n", dr->d_name);
+            if (!strcmp(dr->d_name, ".") || !strcmp(dr->d_name, "..")) continue;
+            sprintf(file_path, "named_fifos/%s", dr->d_name);
+            unlink_fifo(file_path); //delete named pipe pipe
+        }
+    closedir (dir);
+    }
+
+    // then delete directory itself
+    if(rmdir(directory) == -1)
+        perror("Failed to remove directory.\n");
 }
