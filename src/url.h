558abeb7d4b67a49103f077747f51b6e332621e5
Sophia Katsaouni <76869852+sofiaKats@users.noreply.github.com>
2022-05-07 21:31:24 +0300

    addition of libraries and function declaration @ url.h

--- src/url.h
+++ src/url.h
@@ -8,18 +8,22 @@
 #include <fcntl.h> 
 #include <errno.h> 
 #include <unistd.h> 
+#include  <sys/stat.h>
+#include <dirent.h>
 
 #include "list.h"
 
 #define MAXBUFF 1024
 
 // opens given file from inotifywait, reads it and finds all existing urls in file
-void open_file_and_search_for_urls(char* filename);
+void open_file_and_search_for_urls(int start, char* filename);
 
 // given a buffer, funct checks for the existance of urls using regex library
 void find_urls(char** temp_buff, List* url_list);
 
 // creates a .out file, parses through url list and writes the url string as well as no of appearances
-void create_file_and_write_valid_urls(List* url_list, char* filename);
+void create_file_and_write_valid_urls(int start, List* url_list, char* filename);
 
+// empties out_files directory of previous content if there is any
+void empty_directory(char* directory);
 #endif
